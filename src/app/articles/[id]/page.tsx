a05b6e0ccd04a316f1f3b11188f8df811e93f2b6
nagisa725 <kaeno.ane@gmail.com>
2024-01-16 18:04:55 +0900

    ブログ記事詳細ページを作成

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Article = ({params}:{params:{id: string }}) => {
+
+  return (
+    <div className='max-w-3xl mx-auto p-5'>Article
+        <Image 
+        src ='https://source.unsplash.com/collection/1346951/1000×500?sig=1' alt=''
+        width={1280}
+        height={300}
+        />
+        <h1 className='text-4xl text-center md-10 mt-10'>
+            タイトル
+        </h1>
+        <div className='text-lg leading-relaxed text-justify'>
+            <p>本文</p>
+        </div>
+    </div>
+  )
+}
+
+export default Article
