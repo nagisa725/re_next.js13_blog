9eb1205e6263b2710f8fa4dd3a7dc26899c482c8
nagisa725 <kaeno.ane@gmail.com>
2024-01-17 18:32:04 +0900

    詳細記事内容の表示とサムネイル表示の修正

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -5,21 +5,26 @@ import { getDetailArticle } from "@/app/blogAPI";
 const Article = async ({ params }: { params: { id: string } }) => {
   // console.log(params.id)
   const detailArticle = await getDetailArticle(params.id);
-  console.log(detailArticle);
+  // console.log(detailArticle);
   //
 
   return (
     <div className="max-w-3xl mx-auto p-5">
       Article
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000×500?sig=1"
+        src={`https://source.unsplash.com/collection/1346951/1000*500?sig=${detailArticle.id}`} //ここに各ページを示す${detailArticle.id}を入れることで各記事ごとにサムネイルが変わる
         alt=""
         width={1280}
         height={300}
       />
-      <h1 className="text-4xl text-center md-10 mt-10">タイトル</h1>
+      <h1 className="text-4xl text-center md-10 mt-10">
+        {detailArticle.title}
+        {/* blogAPIで引っ張ってきたgetDetailArticleの記事詳細APIをdetailArticleに格納したので第一引数へ
+        posts.jsonで定義したtitleを第二引数に入れる */}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        <p>本文</p>
+        <p>{detailArticle.constent}</p>
+        {/* 上と同義 */}
       </div>
     </div>
   );
--- src/app/compornents/ArticleList.tsx
+++ src/app/compornents/ArticleList.tsx
@@ -1,44 +1,57 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { Article } from '../types'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Article } from "../types";
 
 type ArticleListProps = {
-  articles: Article[];//複数の記事があるので配列[]
-}
-const ArticleList = ({articles}:ArticleListProps) => {
+  articles: Article[]; //複数の記事があるので配列[]
+};
+const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
-          <article className='shadow my-4 flex-col' key={article.id}>
-              <Link href={`articles/${article.id}`} className='hover:opacity-75'>
-                  <Image 
-                  src ='https://source.unsplash.com/collection/1346951/1000×500?sig=1' alt=''
-                  width={1280}
-                  height={300}
-                  />
-              </Link>
-              <div className='bg-white flex flex-col justify-start p-6'>
-                <Link href={`articles/${article.id}`} className='text-blue-700 pb-4 font-bold '>
-                  Technology
-                </Link>
-                <Link href={`articles/${article.id}`} className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'>
-                  {article.title}
-                </Link>
-                <p className='text-sm pd-3 text-slate-900'>
-                  published on {article.createdAt}
-                </p>
-                <Link href={`articles/${article.id}`} className='text-slate-900 pb-6'>
-                  {article.constent}
-                </Link>
-                <Link href={`articles/${article.id}`} className='text-pink-800 hover:text-black'>
-                続きを読む
-                </Link>
-              </div>      
-          </article>
+        <article className="shadow my-4 flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75 ">
+            <Image
+              src={`https://source.unsplash.com/collection/1346951/1000*500?sig=${article.id}`} //ここに各ページを示す${article.id}を入れることで各記事ごとにサムネイルが変わる
+              alt=""
+              width={1280}
+              height={300}
+            />
+          </Link>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link
+              href={`articles/${article.id}`}
+              className="text-blue-700 pb-4 font-bold "
+            >
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pd-3 text-slate-900">
+              published on {article.createdAt}
+            </p>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 pb-6"
+            >
+              {article.constent}
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-pink-800 hover:text-black"
+            >
+              続きを読む
+            </Link>
+          </div>
+        </article>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default ArticleList
+export default ArticleList;
