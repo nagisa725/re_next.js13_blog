a27b4ab6edeee44c597cef3a9ef2ae17e26e5397
nagisa725 <kaeno.ane@gmail.com>
2024-01-17 20:04:01 +0900

    ブログ本文の文字数制限を設け、ブログ記事のコンポーネントを分けた

--- src/app/compornents/ArticleList.tsx
+++ src/app/compornents/ArticleList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Article } from "../types";
+import ArticleCard from "./ArticleCard";
 
 type ArticleListProps = {
   articles: Article[]; //複数の記事があるので配列[]
@@ -10,45 +11,9 @@ const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
-        <article className="shadow my-4 flex-col" key={article.id}>
-          <Link href={`articles/${article.id}`} className="hover:opacity-75 ">
-            <Image
-              src={`https://source.unsplash.com/collection/1346951/1000*500?sig=${article.id}`} //ここに各ページを示す${article.id}を入れることで各記事ごとにサムネイルが変わる
-              alt=""
-              width={1280}
-              height={300}
-            />
-          </Link>
-          <div className="bg-white flex flex-col justify-start p-6">
-            <Link
-              href={`articles/${article.id}`}
-              className="text-blue-700 pb-4 font-bold "
-            >
-              Technology
-            </Link>
-            <Link
-              href={`articles/${article.id}`}
-              className="text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4"
-            >
-              {article.title}
-            </Link>
-            <p className="text-sm pd-3 text-slate-900">
-              published on {article.createdAt}
-            </p>
-            <Link
-              href={`articles/${article.id}`}
-              className="text-slate-900 pb-6"
-            >
-              {article.constent}
-            </Link>
-            <Link
-              href={`articles/${article.id}`}
-              className="text-pink-800 hover:text-black"
-            >
-              続きを読む
-            </Link>
-          </div>
-        </article>
+        <ArticleCard article={article} key={article.id} />
+        /* <article>タグの記述が長くなったので新しくコンポーネントを作りArticleCardをimportでタブ保管
+        各ブログ詳細記事を引っ張るためarticle={article}とkeyを指定*/
       ))}
     </div>
   );
